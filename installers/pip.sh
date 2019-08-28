bf4b0aa7dc4f469b3355031637c414119b67fa44
Ogun ACIK <acikdev@gmail.com>
2019-08-28 21:08:39 +0300

    Add standalone pip installer

---
+++ installers/pip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pipurl="https://bootstrap.pypa.io/get-pip.py"
+
+pip() {
+  pybin=$(command -v python)
+
+  # Download, install and update pip
+  curl -fsSL $pipurl -o /tmp/get-pip.py
+  "${pybin}" /tmp/get-pip.py --user
+
+  pipbin=$(command -v pip)
+
+  export PATH=$PATH:~/.local/bin
+
+  if ! $(grep "export PATH" .bashrc | grep  -w ".local/bin"); then 
+    echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
+  fi
+
+  "${pipbin}" install --upgrade pip
+}
