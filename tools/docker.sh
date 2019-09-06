ba4ca7235a3f12ea1e12c2ba51718185608b129a
Ogun Acik <acikdev@gmail.com>
2019-09-06 15:07:52 +0300

    Make install commands consistent

--- tools/docker.sh
+++ tools/docker.sh
@@ -2,16 +2,17 @@
 
 ubuntu_docker() {
   # Uninstall old versions
-  apt-get remove docker docker-engine docker.io -y
+  apt-get remove -y docker \
+                    docker-engine \
+                    docker.io
 
   apt-get update -y
 
   # Install packages to allow apt to use a repository over HTTPS
-  apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common -y
+  apt-get install -y apt-transport-https \
+                     ca-certificates \
+                     curl \
+                    software-properties-common
 
   # Add Docker’s official GPG key
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -25,19 +26,18 @@ ubuntu_docker() {
   apt-get update -y
 
   # Install the latest version of Docker CE
-  apt-get install docker-ce -y 
+  apt-get install -y docker-ce 
 }
 
 debian_docker() {
   apt-get update -y
 
   # Install packages to allow apt to use a repository over HTTPS
-  apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common -y
+  apt-get install -y apt-transport-https \
+                     ca-certificates \
+                     curl \
+                     gnupg2 \
+                     software-properties-common -y
 
   # Add Docker’s official GPG key
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
@@ -51,10 +51,16 @@ debian_docker() {
   apt-get update -y
 
   # Uninstall old versions
-  apt-get remove docker docker-engine docker.io containerd runc -y
+  apt-get remove -y docker \
+                    docker-engine \
+                    docker.io \
+                    containerd \
+                    runc
 
   # Install the latest version of Docker CE
-   apt-get install docker-ce docker-ce-cli containerd.io -y
+  apt-get install -y docker-ce \
+                     docker-ce-cli \
+                     containerd.io
 }
 
 fedora_docker() {
@@ -68,20 +74,22 @@ fedora_docker() {
     https://download.docker.com/linux/fedora/docker-ce.repo
 
   # Uninstall old versions
-  dnf remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-selinux \
-                  docker-engine-selinux \
-                  docker-engine -y
+  dnf remove -y docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-selinux \
+                docker-engine-selinux \
+                docker-engine
 
   # Install the latest version of Docker CE 
-  dnf -y install docker-ce docker-ce-cli containerd.io
-  
+  dnf install -y docker-ce \
+                 docker-ce-cli \
+                 containerd.io
+
   # Enable and start docker.service
   systemctl enable docker
   systemctl start docker
@@ -91,8 +99,8 @@ centos_docker() {
   # Install required packages. yum-utils,device-mapper-persistent-data
   # lvm2 are required by the devicemapper storage driver.
   yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2 -y
+                 device-mapper-persistent-data \
+                 lvm2
 
   # Set up the stable repository
   yum-config-manager \
@@ -100,13 +108,13 @@ centos_docker() {
     https://download.docker.com/linux/centos/docker-ce.repo
 
   # Uninstall old versions
-  yum remove docker \
-                  docker-common \
-                  docker-selinux \
-                  docker-engine -y
+  yum remove -y docker \
+                docker-common \
+                docker-selinux \
+                docker-engine
 
   # Install the latest version of Docker CE
-  yum install docker-ce -y
+  yum install -y docker-ce 
 
   # Enable and start docker.service
   systemctl enable docker
