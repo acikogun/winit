ac93915eb1ec6e5c605a044b5a014100327b5b99
Ogun Acik <acikdev@gmail.com>
2019-09-08 15:37:39 +0300

    Take pip upgrade commands into a function

--- tools/python3.sh
+++ tools/python3.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+upgrade_pip() {
+  pip3 install --upgrade pip >/dev/null
+
+  # Create a symbolic link to /usr/bin since new pip3
+  # executable is installed into /usr/local/bin/ after upgrade
+  # and sudo doesn't export "/usr/local/bin" path on Centos
+  ln -sf /usr/local/bin/pip3 /usr/bin/pip3
+
+  # Install virtualenv for future use
+  pip3 install --upgrade virtualenv >/dev/null
+}
+
 # Debian handlers
 debian_python3() {
-  apt-get update
+  apt-get update >/dev/null
   apt-get -y install python3
 }
 
 debian_pip3() {
   # Install and upgrade pip
-  apt-get update
+  apt-get update >/dev/null
   apt-get -y install python3-pip
-  pip3 install --upgrade pip
-
-  # Create a symbolic link to /usr/bin since new pip3
-  # executable is installed into /usr/local/bin/ after pip upgrade
-  ln -sf /usr/local/bin/pip3 /usr/bin/pip3
-
-  # Install virtualenv
-  pip3 install --upgrade virtualenv
+  upgrade_pip
 }
 
 debian_9_python3() {
@@ -61,19 +66,7 @@ centos_python3() {
 centos_pip3() {
   # Install and upgrade pip
   yum install -y python36-pip
-  pip3 install --upgrade pip
-
-  # Create a symbolic link to /usr/bin since new pip3
-  # executable is installed into /usr/local/bin/ after upgrade
-  # and sudo doesn't export "/usr/local/bin" path on Centos
-  ln -sf /usr/local/bin/pip3 /usr/bin/pip3
-
-  # Clear pip3 binary location cache since new pip3
-  # binary is installed different location after pip upgrade
-  hash -d pip3
-
-  # Install virtualenv
-  pip3 install --upgrade virtualenv
+  upgrade_pip
 }
 
 centos_7_python3() {
@@ -92,14 +85,7 @@ fedora_python3() {
 fedora_pip3() {
   # Install and upgrade pip
   yum install -y python3-pip
-  pip3 install --upgrade pip
-
-  # Create a symbolic link to /usr/bin since new pip3
-  # executable is installed into /usr/local/bin/ after pip upgrade
-  ln -sf /usr/local/bin/pip3 /usr/bin/pip3
-
-  # Install virtualenv
-  pip3 install --upgrade virtualenv
+  upgrade_pip
 }
 
 fedora_30_python3() {
