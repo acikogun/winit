c296671aee098a4f7df6afc01a6b03af6dced8bf
Ogun Acik <acikdev@gmail.com>
2019-09-08 16:23:25 +0300

    Add ansible and IPython support

--- tools/ansible.sh
+++ tools/ansible.sh
@@ -1,1 +1,48 @@
 #!/bin/bash
+
+ansible_common() {
+  pip3 install --upgrade ansible
+}
+
+# Debian handlers
+debian_ansible() {
+  debian_pip3
+  ansible_common
+}
+
+debian_9_ansible() {
+  debian_ansible
+}
+
+debian_10_ansible() {
+  debian_ansible
+}
+
+# Ubuntu handlers
+ubuntu_16_ansible() {
+  debian_ansible
+}
+
+ubuntu_18_ansible() {
+  debian_ansible
+}
+
+# Centos handlers
+centos_ansible() {
+  centos_pip3
+  ansible_common
+}
+
+centos_7_ansible() {
+  centos_ansible
+}
+
+# Fedora handlers
+fedora_ansible() {
+  fedora_pip3
+  ansible_common
+}
+
+fedora_30_ansible() {
+  fedora_ansible
+}
--- tools/ipython.sh
+++ tools/ipython.sh
@@ -1,1 +1,48 @@
 #!/bin/bash
+
+ipython_common() {
+  pip3 install --upgrade ipython
+}
+
+# Debian handlers
+debian_ipython() {
+  debian_pip3
+  ipython_common
+}
+
+debian_9_ipython() {
+  debian_ipython
+}
+
+debian_10_ipython() {
+  debian_ipython
+}
+
+# Ubuntu handlers
+ubuntu_16_ipython() {
+  debian_ipython
+}
+
+ubuntu_18_ipython() {
+  debian_ipython
+}
+
+# Centos handlers
+centos_ipython() {
+  centos_pip3
+  ipython_common
+}
+
+centos_7_ipython() {
+  centos_ipython
+}
+
+# Fedora handlers
+fedora_ipython() {
+  fedora_pip3
+  ipython_common
+}
+
+fedora_30_ipython() {
+  fedora_ipython
+}
