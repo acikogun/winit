70f210aaa3167930227688c6bd96ecede86f6643
Ogun <acikdev@gmail.com>
2019-11-05 22:57:47 +0300

    Add Terraform to tools

---
+++ tools/terraform.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+terraform_common() {
+  local latest_release_url="https://github.com/hashicorp/terraform/releases/latest"
+  # Get the latest version
+  terraform_version=$(curl -Ls -o /dev/null -w %{url_effective} $latest_release_url | \
+  grep -oE "[^/]+$" | cut -d 'v' -f2 )
+
+  download_file="terraform_${terraform_version}_linux_amd64.zip"
+  download_url="https://releases.hashicorp.com/terraform/${terraform_version}/${download_file}"
+
+  prefix="/usr/bin"
+
+  download_terraform() {
+    echo "Downloading "${download_file}"..."
+    curl -sS ${download_url} -o /tmp/${download_file}
+    echo "Done."
+    echo
+  }
+
+  pre_clean() {
+    rm -rf ${prefix}/terraform
+    rm -rf /tmp/terraform*
+  }
+
+  install_terraform() {
+    echo "Installing Terraform ${terraform_version}..."
+    unzip /tmp/$download_file -d /tmp
+    cp /tmp/terraform $prefix
+    echo "Done."
+    echo
+  }
+
+  pre_clean
+  download_terraform
+  install_terraform
+}
+
+debian_9_terraform() {
+  terraform_common
+}
+
+debian_10_terraform() {
+  terraform_common
+}
+
+ubuntu_16_terraform() {
+  terraform_common
+}
+
+ubuntu_18_terraform() {
+  terraform_common
+}
+
+centos_7_terraform() {
+  terraform_common
+}
