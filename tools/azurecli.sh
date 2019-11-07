2fdd671bd29ce8721920bfbdd43caea732acd14b
Ogun <acikdev@gmail.com>
2019-11-07 17:14:19 +0300

    Add repo key via apt-add

--- tools/azurecli.sh
+++ tools/azurecli.sh
@@ -4,16 +4,16 @@ apt_azurecli() {
   local az_repo=$(lsb_release -cs)
   local repo="deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $az_repo main"
 
-  local repo_dest="/etc/apt/sources.list.d/azure-cli.list"
+  local repo_file="/etc/apt/sources.list.d/azure-cli.list"
 
   local key="https://packages.microsoft.com/keys/microsoft.asc"
   local key_dest="/etc/apt/trusted.gpg.d/microsoft.asc.gpg"
 
   # Add the azurecli distribution URI as a package source
-  echo "${repo}" > "${repo_dest}"
+  echo "${repo}" > "${repo_file}"
 
   # Import the azurecli apt repo signing key
-  curl -sL "${key}" | gpg --dearmor | tee "${key_dest}" >/dev/null 2>&1
+  curl -sL "${key}" | apt-key --keyring "${key_dest}" add - >/dev/null 2>&1
 
   # Install azurecli
   apt-get update >/dev/null 2>&1
