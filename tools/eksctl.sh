f15f596ad3e7322a6dc3717cfd1f6409f0fca27e
Ogun Acik <acikdev@gmail.com>
2020-01-21 20:35:00 +0300

    Don't try to install eksctl if the local version is already the latest

--- tools/eksctl.sh
+++ tools/eksctl.sh
@@ -6,11 +6,23 @@ enable_eksctl_bash_completion() {
 }
 
 eksctl_common() {
+  local api_url="https://api.github.com/repos/weaveworks/eksctl/releases/latest"
+  local eksctl_version
+  eksctl_version=$(curl -sSL "${api_url}" | jq -r .name | awk '{print $2}')
+
+  local is_prerelease
+  is_prerelease=$(curl -sSL "${api_url}" | jq -r .prerelease)
+
   local download_file="eksctl_Linux_amd64.tar.gz"
   local download_url="https://github.com/weaveworks/eksctl/releases/download/latest_release/${download_file}"
+
   local prefix="/usr/bin"
   local download_dest="/tmp/${download_file}"
-  curl -sSL https://api.github.com/repos/weaveworks/eksctl/releases/latest | jq -r .prerelease
+
+  if [[ -f "${prefix}/eksctl" ]]; then
+    local eksctl_installed
+    eksctl_installed=$("${prefix}"/eksctl version | cut -d ',' -f 3 | cut -d '"' -f 2)
+  fi
 
   download_eksctl() {
     echo "Downloading ${download_file}..."
@@ -30,9 +42,14 @@ eksctl_common() {
     echo
   }
 
-  download_eksctl
-  install_eksctl
-  enable_eksctl_bash_completion
+  if [[ "${eksctl_version}" != "${eksctl_installed}" && "${is_prerelease}" == "false" ]]; then
+    download_eksctl
+    install_eksctl
+    enable_eksctl_bash_completion
+  else
+    echo "The latest Eksctl version ${eksctl_version} is already installed."
+    echo
+  fi
 }
 
 debian_9_eksctl() {
