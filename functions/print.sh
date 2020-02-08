ec79f8041c1b16f2e0e9f569bc1dc2afc097fea4
Ogun Acik <acikogun@gmail.com>
2020-02-08 19:46:27 +0300

    Remove unnecessary prefix

--- functions/print.sh
+++ functions/print.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-readonly SGR_RESET="\033[0m"
-readonly SGR_RED="\033[31m"
-readonly SGR_GREEN="\033[32m"
-readonly SGR_YELLOW="\033[33m"
-readonly SGR_CYAN="\033[36m"
+readonly RESET="\033[0m"
+readonly RED="\033[31m"
+readonly GREEN="\033[32m"
+readonly YELLOW="\033[33m"
+readonly CYAN="\033[36m"
 
 print_red() {
-  printf "${SGR_RED}%s${SGR_RESET}\n" "$*"
+  printf "${RED}%s${RESET}\n" "$*"
 }
 
 print_yellow() {
-  printf "${SGR_YELLOW}%s${SGR_RESET}\n" "$*"
+  printf "${YELLOW}%s${RESET}\n" "$*"
 }
 
 print_green() {
-  printf "${SGR_GREEN}%s${SGR_RESET}\n" "$*"
+  printf "${GREEN}%s${RESET}\n" "$*"
 }
 
 print_cyan() {
-  printf "${SGR_CYAN}%s${SGR_RESET}\n" "$*"
+  printf "${CYAN}%s${RESET}\n" "$*"
 }
