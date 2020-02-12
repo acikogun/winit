ade4cb1c84f83c852c7f0179e5deb0273afe9d6d
Ogun Acik <acikogun@gmail.com>
2020-02-12 09:51:03 +0300

    Print a blank line after errors

--- test/common.sh
+++ test/common.sh
@@ -5,7 +5,7 @@ test_command() {
     print_green "$1 installed successfully."
   else
     print_red "ERROR: Unable to install ${pkg}."
-    print_yellow "See ${logdir}/${pkg}.log"
+    print_red "See ${logdir}/${pkg}.log"
   fi
 }
 
