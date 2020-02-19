b9a74d0d8ea13c199bac2990a36166a3e5dca38e
Ogun Acik <acikogun@gmail.com>
2020-02-19 10:51:04 +0300

    Redesign optional test supposed to run after winit

--- test/test_all.sh
+++ test/test_all.sh
@@ -4,13 +4,8 @@ source functions/globals.sh
 source functions/common.sh
 source test/common.sh
 
-test_list=$(get_tool_list)
-
-# Test tool(s)
-for test_pkg in ${test_list}; do
-  if [[ "${test_pkg:0:1}" == "#" ]]; then
-    print_yellow "Skipped ${test_pkg:1}"
-  else
-    "test_${test_pkg}"
-  fi
-done
+if [[ -f "${log_dir:?}/fails.txt" ]]; then
+  print_red "Failed tools:"
+  cat "${log_dir:?}/fails.txt"
+  exit 1
+fi
