c407dc23a9e9db19ab94f21af9e682b5e481f1af
Ogun Acik <acikogun@gmail.com>
2020-02-20 05:54:29 +0300

    Report logs of failed tools

--- test/test_all.sh
+++ test/test_all.sh
@@ -4,8 +4,16 @@ source functions/globals.sh
 source functions/common.sh
 source test/common.sh
 
-if [[ -f "${log_dir:?}/fails.txt" ]]; then
+fail_list=$(get_fail_list)
+
+if [[ -f "${fails_file:?}" ]]; then
   print_red "Failed tools:"
-  cat "${log_dir:?}/fails.txt"
+  cat "${fails_file:?}"
+
+  for failed_tool in ${fail_list}; do
+    if [[ -f "${log_dir:?}/${failed_tool}.log" ]]; then
+      cat "${log_dir:?}/${failed_tool}.log"
+    fi
+  done
   exit 1
 fi
