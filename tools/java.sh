880ad84c6a54148d7d6f22790bc3c2f67acdd90e
Ogun Acik <acikogun@gmail.com>
2020-02-24 13:58:39 +0300

    Refactor duplicated functions and tidy up handlers

--- tools/java.sh
+++ tools/java.sh
@@ -9,24 +9,14 @@ apt_java_repo() {
   add-apt-repository --yes "${apt_repo}"
 }
 
-apt_java8() {
+apt_java() {
+  local java_version="${1}"
   apt_java_repo
 
   apt-get update
 
-  echo "Installing java8..."
-  apt-get install -y adoptopenjdk-8-hotspot
-  echo "Done."
-  echo
-}
-
-apt_java11() {
-  apt_java_repo
-
-  apt-get update
-
-  echo "Installing java8..."
-  apt-get install -y adoptopenjdk-11-hotspot
+  echo "Installing java${java_version}..."
+  apt-get install -y "adoptopenjdk-${java_version}-hotspot"
   echo "Done."
   echo
 }
@@ -46,68 +36,63 @@ gpgkey=${rpm_key}
 EOM
 }
 
-yum_java8() {
+yum_java() {
+  local java_version="${1}"
   yum_java_repo
 
-  echo "Installing java8..."
-  yum install -y adoptopenjdk-8-hotspot
+  echo "Installing java${java_version}..."
+  yum install -y "adoptopenjdk-${java_version}-hotspot"
   echo "Done."
   echo
 }
 
-yum_java11() {
-  yum_java_repo
-
-  echo "Installing java8..."
-  yum install -y adoptopenjdk-11-hotspot
-  echo "Done."
-  echo
-}
-
-ubuntu_16_java8() {
-  apt_java8
-}
-
-ubuntu_18_java8() {
-  apt_java8
-}
-
+# Debian handlers
 debian_9_java8() {
-  apt_java8
-}
-
-debian_10_java8() {
-  apt_java8
-}
-
-centos_7_java8() {
-  yum_java8
-}
-
-centos_8_java8() {
-  yum_java8
-}
-
-ubuntu_16_java11() {
-  apt_java11
-}
-
-ubuntu_18_java11() {
-  apt_java11
+  apt_java 8
 }
 
 debian_9_java11() {
-  apt_java11
+  apt_java 11
+}
+
+debian_10_java8() {
+  apt_java 8
 }
 
 debian_10_java11() {
-  apt_java11
+  apt_java 11
+}
+
+# Ubuntu handlers
+ubuntu_16_java8() {
+  apt_java 8
+}
+
+ubuntu_16_java11() {
+  apt_java 11
+}
+
+ubuntu_18_java8() {
+  apt_java 8
+}
+
+ubuntu_18_java11() {
+  apt_java 11
+}
+
+# CentOS handlers
+centos_7_java8() {
+  yum_java 8
 }
 
 centos_7_java11() {
-  yum_java11
+  yum_java 11
+}
+
+centos_8_java8() {
+  yum_java 8
 }
 
 centos_8_java11() {
-  yum_java11
+  yum_java 11
 }
